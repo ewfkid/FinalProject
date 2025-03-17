ecafedb7fe039d7682e581d2c843176bc3f03482
Abubakirova Maria <mary1978@mail.ru>
2025-03-17 19:55:32 +0300

    обнова

---
+++ app/src/main/java/com/example/spacex/domain/entity/StatusCode.java
@@ -0,0 +1,5 @@
+package com.example.spacex.domain.entity;public class StatusCode {
+    public static final int SUCCESS = 200;
+    public static final int ERROR = 500;
+    public static final int LOADING = 102;
+}
