018555051095cbe515a6aafe3063bd055b0a2be1
ewfkid <nazarova.polina.009@gmail.com>
2025-05-20 20:20:45 +0300

    working version

--- app/src/main/java/com/example/spacex/ui/update_profile/UpdateProfileFragment.java
+++ app/src/main/java/com/example/spacex/ui/update_profile/UpdateProfileFragment.java
@@ -1,15 +1,18 @@
 package com.example.spacex.ui.update_profile;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.spacex.R;
 import com.example.spacex.databinding.FragmentUpdateProfileBinding;
+import com.example.spacex.ui.utils.OnChangeText;
 
 public class UpdateProfileFragment extends Fragment {
 
@@ -28,6 +31,46 @@ public class UpdateProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         binding = FragmentUpdateProfileBinding.bind(view);
         viewModel = new ViewModelProvider(this).get(UpdateProfileViewModel.class);
+        binding.close.setOnClickListener(v -> goBack());
+
+        binding.editEmail.addTextChangedListener(new OnChangeText(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                super.afterTextChanged(s);
+                viewModel.changeEmail(s.toString());
+            }
+        });
+        binding.editName.addTextChangedListener(new OnChangeText(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                super.afterTextChanged(s);
+                viewModel.changeName(s.toString());
+            }
+        });
+        binding.editUsername.addTextChangedListener(new OnChangeText(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                super.afterTextChanged(s);
+                viewModel.changeUsername(s.toString());
+            }
+        });
+        binding.editPhone.addTextChangedListener(new OnChangeText(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                super.afterTextChanged(s);
+                viewModel.changePhone(s.toString());
+            }
+        });
+
+        binding.buttonSave.setOnClickListener(v -> viewModel.save());
+        //TODO:
+        binding.editUserImage.setOnClickListener(v -> {});
+    }
+
+    private void goBack() {
+        final View view = getView();
+        if (view == null) return;
+        Navigation.findNavController(view).navigate(R.id.action_updateProfileFragment_to_profileFragment);
     }
 
     public static Bundle getBundle(@NonNull String id) {
--- app/src/main/java/com/example/spacex/ui/update_profile/UpdateProfileViewModel.java
+++ app/src/main/java/com/example/spacex/ui/update_profile/UpdateProfileViewModel.java
@@ -2,7 +2,32 @@ package com.example.spacex.ui.update_profile;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.spacex.data.repository.UserRepositoryImpl;
+import com.example.spacex.domain.user.GetUserByIdUseCase;
+import com.example.spacex.domain.user.UpdateUserUseCase;
+
 public class UpdateProfileViewModel extends ViewModel {
+    
+    UpdateUserUseCase updateUserUseCase = new UpdateUserUseCase(
+            UserRepositoryImpl.getInstance()
+    );
+    
+    GetUserByIdUseCase getUserByIdUseCase = new GetUserByIdUseCase(
+            UserRepositoryImpl.getInstance()
+    );
 
+    public void changeEmail(String newEmail) {
+    }
 
+    public void changeName(String newName) {
+    }
+
+    public void changeUsername(String newUsername) {
+    }
+
+    public void changePhone(String newPhone) {
+    }
+
+    public void save() {
+    }
 }
